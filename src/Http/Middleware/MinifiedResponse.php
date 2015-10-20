7e4b1ac74d9c837a3fbafa9f97c2ca4cfb029cf4
Jean Francois Chedeville <jef.chedeville@gmail.com>
2015-10-20 15:56:34 -0400

    With Minified HTML

---
+++ src/Http/Middleware/MinifiedResponse.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Jchedev\Http\Middleware;
+
+use Illuminate\Http\Response;
+
+class MinifiedResponse
+{
+    public function handle($request, \Closure $next)
+    {
+        $final = $next($request);
+
+        if ($final instanceof Response) {
+            $final->setContent(minify_html($final->getOriginalContent()));
+        }
+
+        return $final;
+    }
+}

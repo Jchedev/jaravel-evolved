ee000430d06dff5b24810489ad13c8c1d4505ee1
Jean Francois Chedeville <jef.chedeville@gmail.com>
2015-11-30 17:37:55 -0500

    Better management of relations

--- src/Eloquent/Models/Model.php
+++ src/Eloquent/Models/Model.php
@@ -6,9 +6,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Support\Collection;
 use Jchedev\Eloquent\Builders\Builder;
@@ -29,23 +28,46 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model
          * Can define first parameter as true, to always return an object even if NULL
          */
         if (preg_match('/^associated(.*)$/', $method, $method_info) == 1) {
-            $load_empty_object = array_get($parameters, 0, false);
-            if (($found = $this->getRelatedObject($method_info[1], $load_empty_object)) !== false) {
-                return $found;
-            }
-        } else {
-            /*
-             * Proxy method to link an object through a relation.
-             * The action will be selected based on the type of relation
-             */
-            if (preg_match('/^addAssociated(.*)$/', $method, $method_info) == 1 && !is_null($object = array_get($parameters, 0))) {
-                $execute_save_too = array_get($parameters, 0, true);
 
-                return $this->setRelatedObject($method_info[1], $object, $execute_save_too);
-            }
+            return $this->getRelatedObject($method_info[1], array_get($parameters, 0, false));
+        }
+
+        /*
+         * Proxy method to link an object through a relation.
+         * The action will be selected based on the type of relation
+         */
+        if (preg_match('/^addAssociated(.*)$/', $method, $method_info) == 1) {
+            return $this->setRelatedObject($method_info[1], array_get($parameters, 0));
+        }
+
+        /*
+         * Proxy method to link an object through a relation.
+         * The action will be selected based on the type of relation
+         */
+        if (preg_match('/^compareAssociated(.*)$/', $method, $method_info) == 1) {
+            return $this->compareRelatedObject($method_info[1], array_get($parameters, 0));
         }
 
         return parent::__call($method, $parameters);
+    }
+
+    /**
+     * Set the attribute can contain the relation itself
+     *
+     * @param string $key
+     * @param mixed $value
+     * @return $this|bool|null
+     */
+    public function setAttribute($key, $value)
+    {
+        $relation = $this->getRelationObject($key);
+        if (is_a($relation, BelongsTo::class)) {
+            $relation->associate($value);
+
+            return $this;
+        }
+
+        return parent::setAttribute($key, $value);
     }
 
     /**
@@ -60,19 +82,124 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model
     }
 
     /**
-     * Implements the concept of load on the model itself (why only collection??)
+     * Return the relation matching the name or null if doesn't exist
      *
-     * @param array|string $relations
-     * @return $this
+     * @param $relation_name
+     * @return mixed
      */
-    public function load($relations)
+    public function getRelationObject($relation_name)
     {
-        $relations = (array)$relations;
-        foreach ($relations as $relation) {
-            $this->getRelatedObject($relation);
+        if (method_exists($this, $relation_name) === false) {
+            return null;
         }
 
-        return $this;
+        return $this->$relation_name();
+    }
+
+    /**
+     * Get the object from a relation (based on $relation_name) or an empty object (if $return_empty_object = true)
+     *
+     * @param $relation_name
+     * @param bool|false $return_empty_object
+     * @return bool|mixed
+     */
+    protected function  getRelatedObject($relation_name, $return_empty_object = false)
+    {
+        $object_from_relation = ($this->relationLoaded($relation_name) ? $this->getRelation($relation_name) : $this->$relation_name);
+
+        if (is_null($object_from_relation) && $return_empty_object !== false) {
+            $object_from_relation = self::object($object_from_relation);
+        }
+
+        return $object_from_relation;
+    }
+
+    /**
+     * Compare an object with the return of a relation
+     *
+     * @param $relation_name
+     * @param \Illuminate\Database\Eloquent\Model $object
+     * @return bool
+     * @throws \Exception
+     */
+    protected function  compareRelatedObject($relation_name, \Illuminate\Database\Eloquent\Model $object)
+    {
+        $relation = $this->getRelationObject($relation_name);
+
+        $relation_links = $this->relationLink($relation, $object);
+        if (is_null($relation_links)) {
+            throw new \Exception('CompareRelatedObject doesn\'t work on ' . get_class($relation) . ' relations yet');
+        }
+
+        foreach ($relation_links as $key => $value) {
+            if ($this->$key != $value) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Link the object (collection or model) to the relation based on the type of relation
+     *
+     * @param $relation_name
+     * @param $object
+     * @return array
+     */
+    protected function  setRelatedObject($relation_name, $object)
+    {
+        $relation = $this->getRelationObject($relation_name);
+        switch (get_class($relation)) {
+
+            case HasMany::class:
+            case BelongsToMany::class:
+                $objects = is_a($object, Collection::class) ? $object : collect(!is_array($object) ? [$object] : $object);
+                $not_existing = $objects->diff($this->getRelatedObject($relation_name));
+
+                $return = (count($not_existing) != 0) ? $relation->saveMany($not_existing->all()) : [];
+                break;
+
+            case HasOne::class:
+                $return = $relation->save($object);
+                break;
+
+            case BelongsTo::class:
+            case MorphTo::class:
+                $return = $relation->associate($object);
+                $this->save();
+                break;
+        }
+
+        return $return;
+    }
+
+    /**
+     * Return the links between a relation and a model (used for the comparison)
+     *
+     * @param Relation $relation
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return array
+     */
+    protected function  relationLink(Relation $relation, \Illuminate\Database\Eloquent\Model $model)
+    {
+        // todo: find a way to make the many-to-many work too
+
+        switch (get_class($relation)) {
+
+            case BelongsTo::class:
+                return [
+                    $relation->getForeignKey() => $model->id
+                ];
+                break;
+
+            case MorphTo::class:
+                return [
+                    $relation->getMorphType()  => get_class($model),
+                    $relation->getForeignKey() => $model->id
+                ];
+                break;
+        }
     }
 
     /**
@@ -80,27 +207,18 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model
      *
      * @param $query
      * @param $relation_name
-     * @param \Illuminate\Database\Eloquent\Model $model
-     * @return bool
+     * @param \Illuminate\Database\Eloquent\Model $object
+     * @throws \Exception
      */
-    public function     scopeWhereRelation($query, $relation_name, \Illuminate\Database\Eloquent\Model $model)
+    public function     scopeWhereRelation($query, $relation_name, \Illuminate\Database\Eloquent\Model $object)
     {
-        $relation_name = lcfirst($relation_name);
-        if (method_exists($this, $relation_name) === false) {
-            return false;
+        $relation_links = $this->relationLink($this->$relation_name(), $object);
+        if (is_null($relation_links)) {
+            throw new \Exception('WhereRelation doesn\'t work on ' . $relation_name . ' relations yet');
         }
 
-        $relation = $this->$relation_name();
-        switch (get_class($relation)) {
-
-            case BelongsTo::class:
-                $query->where($relation->getForeignKey(), '=', $model->id);
-                break;
-
-            case MorphTo::class:
-                $query->where($relation->getMorphType(), '=', get_class($model));
-                $query->where($relation->getForeignKey(), '=', $model->id);
-                break;
+        foreach ($relation_links as $key => $value) {
+            $query->where($key, '=', $value);
         }
     }
 
@@ -109,148 +227,15 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model
      *
      * @param $query
      * @param $relation_name
-     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param \Illuminate\Database\Eloquent\Model $object
      */
-    public function scopeOrWhereRelation($query, $relation_name, \Illuminate\Database\Eloquent\Model $model)
+    public function     scopeOrWhereRelation($query, $relation_name, \Illuminate\Database\Eloquent\Model $object)
     {
-        $query->orWhere(function ($join) use ($relation_name, $model) {
-            $this->scopeWhereRelation($join, $relation_name, $model);
+        $query->orWhere(function ($join) use ($relation_name, $object) {
+            $this->scopeWhereRelation($join, $relation_name, $object);
         });
     }
 
-    /**
-     * Try to retrieve an object for a relation
-     *
-     * @param $relation_name
-     * @param bool|false $return_empty_object
-     * @return bool|mixed
-     */
-    private function    getRelatedObject($relation_name, $return_empty_object = false)
-    {
-        $relation_name = lcfirst($relation_name);
-        if (method_exists($this, $relation_name) === false) {
-            return false;
-        }
-
-        $object_in_relation = ($this->relationLoaded($relation_name) ? $this->getRelation($relation_name) : $this->$relation_name);
-        if (is_null($object_in_relation) && $return_empty_object === true) {
-            $object_in_relation = self::object($relation_name);
-        }
-
-        return $object_in_relation;
-    }
-
-    /**
-     * Add a new object to a relation whatever the type of the relation is
-     *
-     * @param $relation_name
-     * @param $object
-     * @param bool|true $execute_save
-     * @return array|bool|null
-     */
-    private function    setRelatedObject($relation_name, $object, $execute_save = true)
-    {
-        $relation_name = lcfirst($relation_name);
-        if (method_exists($this, $relation_name) === false) {
-            return false;
-        }
-
-        $relation = $this->$relation_name();
-        switch (get_class($relation)) {
-
-            case BelongsToMany::class:
-                $objects = is_a($object, Collection::class) ? $object : collect(!is_array($object) ? [$object] : $object);
-                $return = $this->addBelongsToManyRelationObject($relation, $objects);
-                break;
-
-            case BelongsTo::class:
-                // todo
-                break;
-
-            case MorphOne::class:
-                // todo
-                break;
-
-            case MorphMany::class:
-                // todo
-                break;
-
-            case MorphTo::class:
-                $return = $this->addMorphToRelationObject($relation, $object, $execute_save);
-                break;
-
-            case HasOne::class:
-                // todo
-                break;
-
-            case HasMany::class;
-                // todo
-                break;
-
-            default:
-                $return = null;
-                break;
-        }
-
-        return $return;
-    }
-
-    /**
-     * Add a new collection of objects to a BelongsToMany collection (if the object is not already associated
-     *
-     * @param BelongsToMany $relation
-     * @param Collection $objects
-     * @return array|null
-     */
-    private function    addBelongsToManyRelationObject(BelongsToMany $relation, Collection $objects)
-    {
-        $return = null;
-        $relation_name = $relation->getRelationName();
-
-        $not_existing = $objects->diff($this->retrieveRelationObject($relation_name));
-        if (count($not_existing) != 0) {
-            $return = $relation->saveMany($not_existing->all());
-        }
-
-        unset($objects, $not_existing);
-
-        return $return;
-    }
-
-    /**
-     * Add a new object for a morphTo relation
-     *
-     * @param MorphTo $relation
-     * @param \Illuminate\Database\Eloquent\Model $object
-     * @param bool|true $save
-     * @return bool|null
-     */
-    private function    addMorphToRelationObject(MorphTo $relation, \Illuminate\Database\Eloquent\Model $object, $save = true)
-    {
-        $relation->associate($object);
-
-        return ($save === true) ? $this->save() : null;
-    }
-
-    /**
-     * Return the correct relations based on an array of values
-     *
-     * @param $values
-     * @param null $prepend
-     * @return array
-     */
-    static function relations($values, $prepend = null)
-    {
-        $values = (!is_array($values) ? [$values] : $values);
-
-        if (!is_null($prepend)) {
-            foreach ($values as $key => $value) {
-                $values[$key] = $prepend . '.' . $value;
-            }
-        }
-
-        return $values;
-    }
 
     /**
      * Return an object in the same namespace than the current class
@@ -273,12 +258,14 @@ abstract class Model extends \Illuminate\Database\Eloquent\Model
      */
     static function classPath($object_name = null)
     {
+        // Retrieve the namespace part of the current class
         $path_class_reference = static::class;
         if (($pos = strrpos($path_class_reference, '\\')) !== false) {
             $path_class_reference = substr($path_class_reference, 0, $pos);
         }
         $path_class_reference .= '\\';
 
+        // Append the namespace with the basename of the object (or the $object_name if it is a string)
         if (!is_null($object_name)) {
             $path_class_reference .= ucfirst($object_name);
         } else {

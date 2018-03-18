51d7ee1b10b8adf93d85e1195b7a785b90abd014
Jef Chedeville <jef.chedeville@gmail.com>
2018-03-18 15:00:21 -0400

    With concept of BuilderServices

---
+++ src/Classes/BuilderServices/BuilderService.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Jchedev\Laravel\Classes\BuilderServices;
+
+use Jchedev\Laravel\Classes\BuilderServices\Modifiers\Modifiers;
+use Illuminate\Database\Eloquent\Builder;
+
+abstract class BuilderService
+{
+    /**
+     * @var
+     */
+    public $identifier_key;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    abstract function builder(): Builder;
+
+    /**
+     * @param \Jchedev\Laravel\Classes\BuilderServices\Modifiers\Modifiers|null $modifiers
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function modifiedBuilder(Modifiers $modifiers = null)
+    {
+        $builder = $this->builder();
+
+        return $this->modifyBuilder($builder, $modifiers);
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     * @param \Jchedev\Laravel\Classes\BuilderServices\Modifiers\Modifiers|null $modifiers
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function modifyBuilder(Builder $builder, Modifiers $modifiers = null)
+    {
+        if (!is_null($modifiers)) {
+            $modifiers->applyToBuilder($builder);
+        }
+
+        return $builder;
+    }
+
+    /**
+     * @param \Jchedev\Laravel\Classes\BuilderServices\Modifiers\Modifiers|null $modifiers
+     * @param array $columns
+     * @return \Illuminate\Database\Eloquent\Collection|static[]
+     */
+    public function get(Modifiers $modifiers = null, $columns = ['*'])
+    {
+        $builder = $this->modifiedBuilder($modifiers);
+
+        return $builder->get($columns);
+    }
+
+    /**
+     * @param $id
+     * @param \Jchedev\Laravel\Classes\BuilderServices\Modifiers\Modifiers|null $modifiers
+     * @param array $columns
+     * @return \Illuminate\Database\Eloquent\Model|null|static
+     */
+    public function find($id, Modifiers $modifiers = null, $columns = ['*'])
+    {
+        $builder = $this->modifiedBuilder($modifiers);
+
+        $builder->where($this->identifierKey(), '=', $id);
+
+        return $builder->first($columns);
+    }
+
+    /**
+     * @param \Jchedev\Laravel\Classes\BuilderServices\Modifiers\Modifiers|null $modifiers
+     * @param array $columns
+     * @return \Illuminate\Database\Eloquent\Model|null|static
+     */
+    public function first(Modifiers $modifiers = null, $columns = ['*'])
+    {
+        $builder = $this->modifiedBuilder($modifiers);
+
+        return $builder->first($columns);
+    }
+
+    /**
+     * @param \Jchedev\Laravel\Classes\BuilderServices\Modifiers\Modifiers|null $modifiers
+     * @param string $columns
+     * @return int
+     */
+    public function count(Modifiers $modifiers = null, $columns = '*')
+    {
+        $builder = $this->modifiedBuilder($modifiers);
+
+        return $builder->count($columns);
+    }
+
+    /**
+     * @param \Jchedev\Laravel\Classes\BuilderServices\Modifiers\Modifiers|null $modifiers
+     * @return string
+     */
+    public function toSql(Modifiers $modifiers = null)
+    {
+        $builder = $this->modifiedBuilder($modifiers);
+
+        return $builder->toSql();
+    }
+
+    /**
+     * @return string
+     */
+    public function identifierKey()
+    {
+        if (!is_null($this->identifier_key)) {
+            return $this->identifier_key;
+        }
+
+        return $this->builder()->getModel()->getKeyName();
+    }
+}
---
+++ src/Classes/BuilderServices/Modifiers/Modifiers.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Classes\BuilderServices\Modifiers;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class Modifiers
+{
+    /**
+     * @var  array
+     */
+    private $filters;
+
+    /**
+     * @var int
+     */
+    private $offset;
+
+    /**
+     * @var int
+     */
+    private $limit;
+
+    /**
+     * @param null $offset
+     * @return $this
+     */
+    public function offset($offset = null)
+    {
+        $this->offset = $offset;
+
+        return $this;
+    }
+
+    /**
+     * @param null $limit
+     * @return $this
+     */
+    public function limit($limit = null)
+    {
+        $this->limit = $limit;
+
+        return $this;
+    }
+
+    /**
+     * @param array $filters
+     * @return $this
+     */
+    public function filters(array $filters = [])
+    {
+        $this->filters = $filters;
+
+        return $this;
+    }
+
+    /**
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function applyToBuilder(Builder $builder)
+    {
+        if (!is_null($this->limit)) {
+            $builder->take($this->limit < 0 ? 0 : $this->limit);
+
+            $builder->skip($this->offset);
+        }
+    }
+}

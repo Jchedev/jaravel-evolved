529cab720db4cfd994fd8d7dc95ae02dae27158e
Jef Chedeville <jef.chedeville@gmail.com>
2018-12-27 17:30:57 -0500

    Add Coordinates System + Trait

---
+++ src/Classes/GPS/GPSCoordinates.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Jchedev\Laravel\Classes;
+
+class GPSCoordinates
+{
+    protected $latitude;
+
+    protected $longitude;
+
+    /**
+     * GPSCoordinates constructor.
+     *
+     * @param null $latitude
+     * @param null $longitude
+     */
+    public function __construct($latitude = null, $longitude = null)
+    {
+        $this->latitude = $latitude;
+
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isValid()
+    {
+        return !is_null($this->latitude) && !is_null($this->longitude);
+    }
+
+    /**
+     * @return null
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @return null
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+}
---
+++ src/Traits/HasGPSCoordinates.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jchedev
+ * Date: 2018-12-27
+ * Time: 17:22
+ */
+
+namespace Jchedev\Laravel\Traits;
+
+use Jchedev\Laravel\Classes\GPSCoordinates;
+
+trait HasGPSCoordinates
+{
+    public function getGPSCoordinates()
+    {
+        return new GPSCoordinates($this->latitude, $this->longitude);
+    }
+}

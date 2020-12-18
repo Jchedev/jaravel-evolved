d55a4f7db6574c4835c82e665342a23570816431
Jean Francois Chedeville <jef.chedeville@gmail.com>
2020-12-18 13:57:41 -0800

    Missing addMeta on Resource

--- src/Http/Resources/Resource.php
+++ src/Http/Resources/Resource.php
@@ -3,6 +3,7 @@
 namespace Jchedev\Laravel\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
 
 class Resource extends JsonResource
 {
@@ -13,5 +14,17 @@ class Resource extends JsonResource
     public static function collection($resource)
     {
         return new Collection($resource, static::class);
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @return \Jchedev\Laravel\Http\Resources\Resource
+     */
+    public function addMeta($key, $value)
+    {
+        $additional = Arr::add($this->additional, 'meta.' . $key, $value);
+
+        return $this->additional($additional);
     }
 }
